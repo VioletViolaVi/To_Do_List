428f65f3af88a346e26c0cd6eab0028f5dea49c7
VioletViolaVi <vivian.v18723@gmail.com>
2021-02-01 20:18:53 +0000

    add ability to cross out tasks using javascript.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,41 +1,56 @@
 document.addEventListener("DOMContentLoaded", function () {
   let addBtn = document.getElementById("addBtn");
-  let taskList = document.getElementById("tasksContainer");
-  let taskInputField = document.getElementById("addTaskInput");
+  let addedTask = document.createElement("li");
 
+  // adds task, tick box and bin btn
   addBtn.addEventListener("click", function () {
-      
-    let addedTask = document.createElement("li");
+    let taskList = document.getElementById("tasksContainer");
+    let taskInputField = document.getElementById("addTaskInput");
 
+    // adds task
     function addTaskText() {
       addedTask.innerText = taskInputField.value;
       taskList.appendChild(addedTask);
     }
     addTaskText();
 
+    // adds tick box
     function addTickBox() {
       let addTickBox = document.createElement("input");
+      addTickBox.className = "to_tick";
       addTickBox.setAttribute("type", "checkbox");
       addedTask.appendChild(addTickBox);
+      lineThrough();
     }
     addTickBox();
 
-    function addDeleteBtn() {
-        let deleteBtn = document.createElement("i");
-        deleteBtn.className = "far fa-trash-alt";
-        addedTask.appendChild(deleteBtn);
+    // adds bin btn
+    function addBinBtn() {
+      let binBtn = document.createElement("i");
+      binBtn.className = "far fa-trash-alt";
+      addedTask.appendChild(binBtn);
     }
-    addDeleteBtn();
+    addBinBtn();
 
+    // clears input field
     function clearInputField() {
       taskInputField.value = "";
     }
     clearInputField();
   });
 
+  // crosses out tasks
+  function lineThrough() {
+    let tickBox = document.querySelector(".to_tick");
 
-
-
+    tickBox.addEventListener("click", function () {
+      if (tickBox.checked == true) {
+        addedTask.style.textDecoration = "line-through";
+      } else {
+        addedTask.style.textDecoration = "none";
+      }
+    });
+  }
 
 
 
